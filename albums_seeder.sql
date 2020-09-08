875a8190c161aa37f25f36e9e2055cf1239d28ec
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-09-07 19:30:02 -0500

    done adding albums values

---
+++ albums_seeder.sql
@@ -0,0 +1,19 @@
+USE codeup_test_db;
+-- Use INSERT to add records for all the albums from this list on Wikipedia that claim over 30 million sales
+-- (the first two tables). For sales data, use the 'sales certification' column of the tables, not 'claimed sales'.
+-- For artists listed with 'Various Artists', just use the primary artist's name.
+-- First write your queries as separate INSERT statements for each record and test. You should see no output.
+-- Refactor your script to use a single INSERT statement for all the records and test it again. Again, this should not
+-- generate any output.
+
+INSERT INTO albums (artist, name, content, release_date,genre,sales)
+VALUES ('Michael Jackson', 'Thriller', 1982, 'Pop, post-disco, funk, rock', 66),
+        ('AC/DC','Back in Black', 1980, 'Hard rock ', 50),
+        ('Meat Loaf','Bat out of hell', 1977, 'Hard rock, glam rock, progressive rock', 50),
+        ('Pink Floyd','The Dark Side of the Moon ', 1973, 'Progressive rock', 45),
+        ('Whitney Houston / Various artists	','The Bodyguard', 1992, 'R&B, soul, pop, soundtrack ', 45),
+        ('Eagles','Their Greatest Hits (1971–1975)	', 1976, 'Country rock, soft rock, folk rock ', 42),
+        ('Bee Gees / Various artists ','Saturday Night Fever ', 1977, 'Disco ', 40),
+        ('Fleetwood Mac	','Rumours ', 1977, 'Soft rock', 40),
+        ('Shania Twain	','Come On Over	', 1997, 'Country pop', 40),
+      ;
