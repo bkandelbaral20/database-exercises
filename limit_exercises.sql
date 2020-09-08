684553b0336b05abedded28f8a241779905e4d57
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-09-08 15:20:23 -0500

    completed limit exercises

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -5,9 +5,9 @@ USE employees;
 SELECT DISTINCT title FROM titles;
 
 -- List the first 10 distinct last name sorted in descending order. Your result should look like this:
-SELECT  last_name
+SELECT DISTINCT last_name
 FROM employees
-WHERE last_name LIMIT 10;
+ORDER BY last_name DESC LIMIT 10;
 
 --
 --
@@ -24,11 +24,10 @@ WHERE last_name LIMIT 10;
 
 -- Find your query for employees born on Christmas and hired in the 90s from order_by_exercises.sql.
 -- Update it to find just the first 5 employees. Their names should be:
-select hire_date,birth_date
-from employees
-where hire_date like '199% ' and birth_date like '%-12-25'
-ORDER BY birth_date , hire_date desc
-limit 5 ;
+SELECT *
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' AND birth_date LIKE '%12-25'
+ORDER BY birth_date ASC, hire_date DESC LIMIT 5;
 
 -- Khun Bernini
 -- Pohua Sudkamp
@@ -39,6 +38,10 @@ limit 5 ;
 -- Try to think of your results as batches, sets, or pages. The first five results are your first page.
 -- The five after that would be your second page, etc. Update the query to find the tenth page of results.
 -- The employee names should be:
+SELECT *
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' AND birth_date LIKE '%12-25'
+ORDER BY birth_date ASC, hire_date DESC LIMIT 5 OFFSET 45;
 
 
 -- Piyawadee Bultermann
