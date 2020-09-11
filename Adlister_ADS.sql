fe5c9a4c4cdf0fcf27e9e056e6b47804a857492b
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-09-11 15:16:48 -0500

    completed exercises

--- Adlister_ADS.sql
+++ Adlister_ADS.sql
@@ -5,24 +5,28 @@
 
 USE codeup_test_db;
 
-Create TABLE users(
+Create TABLE IF NOT EXISTS users(
 id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+user_name VARCHAR(200) NOT NULL,
 email varchar(50) NOT NUll,
 password varchar(20) NOt NULL,
 primary key (id)
 );
 
-CREATE TABLE ads (
+CREATE TABLE IF NOT EXISTS ads (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     title VARCHAR(50) NOT NULL,
     description TEXT NOT NULL,
     PRIMARY KEY (id)
 );
+
 CREATE table categories(
  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
- description varchar(200) NOT NULL,
+ cat_name varchar(200) NOT NULL,
  primary key(id)
 );
+
+--____________ LINKING ALL THE TABLE TOGETHER_______
 
 CREATE TABLE ads_users(
 ads_id INT UNSIGNED NOT NULL,
@@ -41,8 +45,30 @@ ads_id INT UNSIGNED NOT NULL,
 -- Write SQL queries to answer the following questions:**********************
 
 -- For a given ad, what is the email address of the user that created it?
-SELECT email
-FROM user
+select * from ads
+where id in (
+  select id from users
+  where email = ''
+  );
+
 -- For a given ad, what category, or categories, does it belong to?
+
 -- For a given category, show all the ads that are in that category.
 -- For a given user, show all the ads they have posted.
+
+
+INSERT INTO users(id, user_name, email, password)
+ VALUES (1, 'Tommy',   'password0','TOm23@gmail.com'),
+  (2, 'Tracy',  'password1','abc@gmail.com'),
+  (3, 'Trevor',  'password2','xyz@gmail.com');
+
+
+INSERT INTO categories(id,cat_name)
+VALUES
+  (1,'TOOLS'),
+  (2,'JEWELRY'),
+  (3,'ELECTRONICS'),
+  (4,'PlANTS'),
+  (5,'HANDICRAFTS'),
+  (6,'DIY'),
+  (7,'KITCHEN APPLIANCES');
